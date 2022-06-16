84f9ee807d08fac1e6e91715df258f99ee396965
aaa <jsy10301@daum.net>
2022-06-16 09:27:09 +0900

    final version

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "BBANTO",
       home: Grade(),
@@ -23,13 +21,127 @@ class Grade extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber.shade800,
+      backgroundColor: Colors.amber[800],
       appBar: AppBar(
-        elevation: 0,
-        title: Text("BBANTO"),
-        backgroundColor: Colors.amber.shade700,
-        centerTitle: true,
-      ),
+          title: const Text("BBANTO"),
+          backgroundColor: Colors.amber.shade700,
+          centerTitle: true,
+          elevation: 0),
+      body: Padding(
+          padding: EdgeInsets.fromLTRB(30, 40, 0, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage("assets/chef.jpg"),
+                  radius: 60,
+                ),
+              ),
+              Divider(
+                height: 60,
+                color: Colors.grey[850],
+                thickness: 0.5,
+                endIndent: 30,
+              ),
+              Text(
+                "NAME",
+                style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "BBANTO",
+                style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Text(
+                "BBANTO POWER LEVEL",
+                style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "14",
+                style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                children: [
+                  Icon(Icons.check_circle_outline),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    "using lightsaber",
+                    style: TextStyle(
+                      fontSize: 16,
+                      letterSpacing: 1,
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Icon(Icons.check_circle_outline),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    "face hero tattoo",
+                    style: TextStyle(
+                      fontSize: 16,
+                      letterSpacing: 1,
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Icon(Icons.check_circle_outline),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    "fire flames",
+                    style: TextStyle(
+                      fontSize: 16,
+                      letterSpacing: 1,
+                    ),
+                  )
+                ],
+              ),
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage("assets/infron.png"),
+                  radius: 50,
+                  backgroundColor: Colors.amber[800],
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,1 +1,2 @@
 
+// https://github.com/poseidonTest/bbanto.git
